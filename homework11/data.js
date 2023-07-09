6ce4206883bc531b966726ad87d13f173df4e8eb
Sergey Batenin <inferno.87@mail.ru>
2023-07-09 16:41:22 +0300

    homework 11 неоптимизированный вариант

--- homework11/data.js
+++ homework11/data.js
@@ -1,43 +1,49 @@
 const cardsInfo = `
 [
     {
+        "id" : 1,
         "image" : "./images/goods_img1.png",
-        "name" : "ELLERY X M'O CAPSULE",
+        "name" : "ELLERY X M'O CAPSULE #1",
         "description" : "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
         "currency" : "$",
         "price" : "52.00"
     },
     {
+        "id" : 2,
         "image" : "./images/goods_img2.png",
-        "name" : "ELLERY X M'O CAPSULE",
+        "name" : "ELLERY X M'O CAPSULE #2",
         "description" : "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
         "currency" : "$",
         "price" : "52.00"
     },
     {
+        "id" : 3,
         "image" : "./images/goods_img3.png",
-        "name" : "ELLERY X M'O CAPSULE",
+        "name" : "ELLERY X M'O CAPSULE #3",
         "description" : "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
         "currency" : "$",
         "price" : "52.00"
     },
     {
+        "id" : 4,
         "image" : "./images/goods_img4.png",
-        "name" : "ELLERY X M'O CAPSULE",
+        "name" : "ELLERY X M'O CAPSULE #4",
         "description" : "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
         "currency" : "$",
         "price" : "52.00"
     },
     {
+        "id" : 5,
         "image" : "./images/goods_img5.png",
-        "name" : "ELLERY X M'O CAPSULE",
+        "name" : "ELLERY X M'O CAPSULE #5",
         "description" : "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
         "currency" : "$",
         "price" : "52.00"
     },
     {
+        "id" : 6,
         "image" : "./images/goods_img6.png",
-        "name" : "ELLERY X M'O CAPSULE",
+        "name" : "ELLERY X M'O CAPSULE #6",
         "description" : "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
         "currency" : "$",
         "price" : "52.00"
--- homework11/script.js
+++ homework11/script.js
@@ -3,9 +3,10 @@ const data = JSON.parse(cardsInfo);
 const goodsEl = document.querySelector('.main__goods');
 
 data.forEach(element => {
-    // создаем общий блок карточки и добавлям класс
+    // создаем общий блок карточки и добавлям класс, id
     const itemEl = document.createElement("div");
     itemEl.classList.add("item");
+    itemEl.setAttribute("id", element.id);
 
     // создаем блок с фотографией товара для карточки
     const itemPhotoEl = document.createElement("div");
@@ -50,4 +51,133 @@ data.forEach(element => {
     
     // Добавляем карточку с товаром для отображения на сайте
     goodsEl.append(itemEl);
+
+    // Добавляем блок с кнопкой при эффекте наведения
+    const itemBtnLink = document.createElement("a");
+    const itemBtnEl = document.createElement("div");
+    itemBtnEl.classList.add("item__button");
+    itemBtnEl.textContent = "Add to Cart";
+    itemBtnEl.insertAdjacentHTML("afterbegin", shoppingCartSvg);
+    itemBtnLink.append(itemBtnEl);
+
+    itemPhotoEl.append(itemBtnLink);
+});
+
+
+// добавляем событие, чтобы при клике на товар он добавлялся в корзину
+const shopCartEl = document.querySelector('.cart-content');
+const addItemToShopCartBtns = document.querySelectorAll('.item__button');
+// Элемент где непосредственно будут храниться карточки товаров
+const cartsElement = document.querySelector('.cart');
+
+addItemToShopCartBtns.forEach(btn => {
+    btn.addEventListener('click', function (e) {
+        // получаем исходную карточку товара по которой произвели нажатие
+        const currItem = btn.parentElement.parentElement.parentElement;
+        const imgItem = currItem.firstChild.firstChild;
+        
+        // создаем блок для карточки товара в корзине, добавляем класс
+        const productCard = document.createElement("div");
+        productCard.classList.add("cart__product");
+        // создаем раздел с фотографией товара
+        const productImage = document.createElement("img");
+        productImage.classList.add("product__image");
+        productImage.src = imgItem.src;
+        productImage.alt = "product image";
+
+        // Создаем раздел с описанием товара
+        const productDescription = document.createElement("div");
+        productDescription.classList.add("product__description");
+        // Создание раздела с названием товара и кнопкой удаления
+        const productName = document.createElement("div");
+        productName.classList.add("product__name");
+        const productNameLink = document.createElement("a");
+        productNameLink.classList.add("product__link");
+        productNameLink.href = "#";
+        productNameLink.textContent = currItem.lastChild.firstChild.textContent;
+        const productRemoveButton = document.createElement("button");
+        productRemoveButton.classList.add("product__remove-button");
+        productRemoveButton.insertAdjacentHTML("afterbegin", removeButtonSvg);
+
+        productName.append(productNameLink);
+        productName.append(productRemoveButton);
+
+        // Создаем раздел с характеристиками товара
+        const productAttributes = document.createElement("div");
+        productAttributes.classList.add("product__attributes");
+        // Раздел цены - общий блок
+        const productPriceP = document.createElement("p");
+        productPriceP.textContent = "Price: ";
+        // Раздел цены - сумма
+        const productPriceAmount = document.createElement("span");
+        productPriceAmount.classList.add("product__attributes_price");
+        productPriceAmount.textContent = currItem.lastChild.lastChild.lastChild.textContent;
+        // Раздел цены - валюта
+        const productPriceCurrency = document.createElement("span");
+        productPriceCurrency.classList.add("product__price_currency");
+        productPriceCurrency.textContent = currItem.lastChild.lastChild.firstChild.textContent;
+        // Собираем блок с ценой
+        productPriceAmount.insertAdjacentElement("afterbegin", productPriceCurrency);
+        productPriceP.append(productPriceAmount);
+
+        // Раздел цвета товара
+        const productColor = document.createElement("p");
+        productColor.textContent = "Color: Red";
+
+        // Раздел размера товара
+        const productSize = document.createElement("p");
+        productSize.textContent = "Size: Xl";
+
+        // Форма с вводом количества товара
+        const productQuantityForm = document.createElement("form");
+        productQuantityForm.classList.add("product__attributes_input-box");
+        productQuantityForm.textContent = "Quantity:";
+        
+        const productQuantityInput = document.createElement("input");
+        productQuantityInput.classList.add("product__attributes_input");
+        productQuantityInput.type = "number";
+        productQuantityInput.value = 1;
+
+        productQuantityForm.append(productQuantityInput);
+
+        productAttributes.append(productPriceP);
+        productAttributes.append(productColor);
+        productAttributes.append(productSize);
+        productAttributes.append(productQuantityForm);
+        
+        productDescription.append(productName);
+        productDescription.append(productAttributes);
+
+        productCard.append(productImage);
+        productCard.append(productDescription);
+
+        cartsElement.append(productCard);
+        // Делаем нашу корзину видимой с добавленным товаром
+        shopCartEl.classList.remove("invisible");
+
+        // Добавляем событие удаление элемента по нажатию по кнопке
+        productRemoveButton.addEventListener('click', function (e) {            
+            productCard.remove();
+            // Если карточек в корзине больше нет, тогда скрываем корзину
+            if (cartsElement.children.length == 0) {
+                shopCartEl.classList.add("invisible");
+            }        
+        });
+    });
+});
+
+
+// добавляем событие для кнопок удаления товаров из корзины
+const removeBtns = document.querySelectorAll('.product__remove-button');
+removeBtns.forEach(btn => {
+    btn.addEventListener('click', function (e) {
+        // Определяем родительский блок карточки товара
+        const currentItem = e.currentTarget.parentElement.parentElement.parentElement;
+
+        currentItem.remove();
+
+        if (cartsElement.children.length == 0) {
+            shopCartEl.classList.add("invisible");
+        }        
+    });
 });
